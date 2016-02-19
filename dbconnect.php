be5229a5d3c6c8464002e42d91e55567f99e0a4d
Akienzo <ringstyle2@gmail.com>
2016-02-19 21:35:47 +0900

    dbconnect.php database name fixed

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <?php
-$db = mysqli_connect('mysql1.php.xdomain.ne.jp', 'ringstyle09_root', 'nishimu1') or die(mysqli_connect_error());
+$db = mysqli_connect('mysql1.php.xdomain.ne.jp', 'ringstyle09_root', 'nishimu1','ringstyle09_shop') or die(mysqli_connect_error());
 mysqli_set_charset($db, 'utf8');
 ?>
